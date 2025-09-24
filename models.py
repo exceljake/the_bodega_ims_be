0a36c557f73ddc26281c07b96b0f7704f4f888c0
eaquino <eaquino@iripple.com>
2025-09-24 20:44:30 +0800

    added shops model

--- models.py
+++ models.py
@@ -3,9 +3,18 @@ from app import db
 class Product(db.Model):
     __tablename__ = "products"   
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), unique=True, nullable=False)
     sku = db.Column(db.String(50), unique=True, nullable=False)
     quantity = db.Column(db.Integer, nullable=False, default=0)
     price = db.Column(db.Float, nullable=False, default=0.0)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    
+class Shop(db.Model):
+    __tablename__ = 'shops'
+    id = db.Column(db.Integer, primary_key=True)
+    shop_name = db.Column(db.String(100), unique=True, nullable=False)
+    address = db.Column(db.Text)
+    contact_number = db.Column(db.String(20))
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
