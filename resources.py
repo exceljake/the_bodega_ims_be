841c0e5855f227fc2a75c9c2f94d151b20091a4b
eaquino <eaquino@iripple.com>
2025-09-25 17:54:52 +0800

    shops model

--- resources.py
+++ resources.py
@@ -2,8 +2,8 @@
 from flask import request
 from flask_restful import Resource
 from app import db
-from models import Product
-from schemas import product_schema, products_schema
+from models import Product, Shop
+from schemas import product_schema, products_schema, shop_schema, shops_schema
 
 class ProductListResource(Resource):
     def get(self):
@@ -13,7 +13,7 @@ class ProductListResource(Resource):
     def post(self):
         data = request.json
         existing_sku = Product.query.filter_by(sku=data["sku"]).first()
-        existing_name = Product.query.filter_by(sku=data["sku"]).first()
+        existing_name = Product.query.filter_by(name=data["name"]).first()
         if existing_sku:
             return {"message": "SKU already existing"}, 422
         if existing_name:
@@ -46,3 +46,41 @@ class ProductResource(Resource):
         db.session.delete(product)
         db.session.commit()
         return {"message": "Product deleted successfully"}, 204
+    
+class ShopListResource(Resource):
+    def get(self):
+        shops = Shop.query.all()
+        return shops_schema.dump(shops)
+    
+    def post(self):
+        data = request.json
+        existing_name = Shop.query.filter_by(shop_name=data["shop_name"]).first()
+        if existing_name:
+            return {"message": "Shop name already existing"}, 422
+        new_shop = Shop(
+            shop_name=data["shop_name"],
+            address=data["address"],
+            contact_number=data["contact_number"]
+        )
+        db.session.add(new_shop)
+        db.session.commit()
+        return shop_schema.dump(new_shop), 201
+    
+class ShopResource(Resource):
+    def get(self, id):
+        shop = Shop.query.get_or_404(id)
+        return shop_schema.dump(shop)
+
+    def put(self, id):
+        shop = Shop.query.get_or_404(id)
+        data = request.json
+        for key, value in data.items():
+            setattr(shop, key, value)
+        db.session.commit()
+        return shop_schema.dump(shop)
+
+    def delete(self, id):
+        shop = Shop.query.get_or_404(id)
+        db.session.delete(shop)
+        db.session.commit()
+        return {"message": "Shop deleted successfully"}, 204
--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 # schemas.py
 from app import ma
-from models import Product
+from models import Product, Shop
 
 class ProductSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -18,3 +18,19 @@ class ProductSchema(ma.SQLAlchemyAutoSchema):
 
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
+
+class ShopSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Shop
+        load_instance = True
+        fields = (
+            "id",
+            "shop_name",
+            "address",
+            "contact_number",
+            "created_at",  
+            "updated_at"
+        )
+
+shop_schema = ShopSchema()
+shops_schema = ShopSchema(many=True)
